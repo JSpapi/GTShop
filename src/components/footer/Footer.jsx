19333e7f41f2cffd417ff1eeb05470035b4f4c97
JSpapi <104095661+JSpapi@users.noreply.github.com>
2023-03-16 17:43:39 +0500

    finished:footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
-
+import React from "react";
+import { logo } from "../../assets";
+import { getContext } from "../../Context";
 const Footer = () => {
+  const { socialMedia } = getContext();
   return (
-	 <footer>Footer</footer>
-  )
-}
+    <footer className={`w-full pt-5 hidden md:block`}>
+      <div className="container">
+        <div className={`py-5 flex justify-between items-start`}>
+          <div className={`flex items-center`}>
+            <div className={`w-[70px] mr-[100px]`}>
+              <img src={logo} alt="logo" className={`w-full object-contain`} />
+            </div>
+            <div className={``}>
+              <h4
+                className={`text-[18px] font-medium leading-[24px] text-white mb-3`}
+              >
+                Контакты
+              </h4>
+              <div className={`flex flex-col items-start gap-3`}>
+                <a
+                  href="mailto:GtStore@gmail.com"
+                  className={`font-light text-[14px]`}
+                >
+                  GtStore@gmail.com
+                </a>
+                <a href="tel:998941231234" className={`font-light text-[14px]`}>
+                  +99894 123 12 34
+                </a>
+              </div>
+            </div>
+            {/* <p className={`font-light text-[13px] mt-5`}>
+                {new Date().getFullYear()} &copy; Ahror Kurban
+              </p> */}
+          </div>
+          <div className={``}>
+            <h4
+              className={`text-[18px] font-medium leading-[24px] text-white mb-5`}
+            >
+              Мы в социальных сетях
+            </h4>
+            <div className={` flex items-start justify-center gap-5`}>
+              {socialMedia.map(({ id, path, icon }) => (
+                <a href={path} key={id}>
+                  {icon}
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
